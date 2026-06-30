6ded3a8c1054da13fa19a1c39ddcdfd8a92eb9c8
ozythings <54993448+ozythings@users.noreply.github.com>
2026-06-30 07:57:32 +0300

    feat: add lazy loading routes and app shell with navigation

---
+++ src/app/features/books/books.routes.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+import { UnsavedChangesGuard } from '../../core/guards/unsaved-changes.guard';
+
+export const booksRoutes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/books-list/books-list.component').then(
+        m => m.BooksListComponent
+      ),
+  },
+  {
+    path: 'ekle',
+    loadComponent: () =>
+      import('./pages/books-form/books-form.component').then(
+        m => m.BooksFormComponent
+      ),
+    canDeactivate: [UnsavedChangesGuard],
+  },
+  {
+    path: ':id/duzenle',
+    loadComponent: () =>
+      import('./pages/books-form/books-form.component').then(
+        m => m.BooksFormComponent
+      ),
+    canDeactivate: [UnsavedChangesGuard],
+  },
+];
